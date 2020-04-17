5f92b090e8285b9c61ab22b6feb1a05ef4b4049e
Admin <Nguyenthanhnoem2k27@gmail.com>
2020-04-17 08:25:01 +0700

    debug err: limited to 5 upload directories

--- index.js
+++ index.js
@@ -54,9 +54,15 @@ app.post('/upload',
         // })
 
         uploadManyFiles(req, res, function (err) {
-            if(err){
-                res.send('' + err)
-            }else {
+            if (err) {
+                if (req.files.length <= 0) {
+                    res.send('không có file nào tải lên')
+                } else if (req.files.length > 5) {
+                    res.send('chỉ được tải lên giới hạn 5 file')
+                } else {
+                    res.send('' + err)
+                }
+            } else {
                 res.send('thanh cong roi kiem tra thu muc uploads')
             }
         })
